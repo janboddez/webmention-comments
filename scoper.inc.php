e45543d4316bb0c18cec0f8faf80bde993306cba
Jan Boddez <jan.boddez@gmail.com>
2020-04-10 11:18:51 +0200

    Update README.md

--- scoper.inc.php
+++ scoper.inc.php
@@ -6,10 +6,6 @@ use Isolated\Symfony\Component\Finder\Finder;
 
 return [
     'prefix' => 'Webmention_Comments',
-    // By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
-    // directory. You can however define which files should be scoped by defining a collection of Finders in the
-    // following configuration key.
-    //
     // For more see: https://github.com/humbug/php-scoper#finders-and-paths
     'finders' => [
         Finder::create()
@@ -28,8 +24,5 @@ return [
         Finder::create()->append([
             'composer.json',
         ]),
-    ],
-    'files-whitelist' => [
-        'vendor/indieweb/mention-client/phpunit'
-    ],
+    ]
 ];
